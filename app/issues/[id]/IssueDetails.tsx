de01640c5ef977e436052cf8c0d51c6b2f478e8a
jnjambrin0 <125131418+jnjambrin0@users.noreply.github.com>
2023-11-16 11:11:46 +0100

    Refactor issue page layout and components

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,22 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Heading, Flex, Card, Text } from "@radix-ui/themes";
+import React from "react";
+import ReactMarckdown from "react-markdown";
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <Flex className="space-x3" my={"2"}>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose" mt={"4"}>
+        <ReactMarckdown>{issue.description}</ReactMarckdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
