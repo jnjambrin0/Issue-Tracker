f5f38d0aad8721bf8f3bdcb35d0e62403e805359
jnjambrin0 <125131418+jnjambrin0@users.noreply.github.com>
2023-11-16 10:52:58 +0100

    Refactor: imports for loading components

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,6 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import { Heading, Flex, Card } from "@radix-ui/themes";
+import { Flex, Card } from "@radix-ui/themes";
 import React from "react";
-import ReactMarckdown from "react-markdown";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
 
 const LoadingIssueDetailPage = () => {
   return (
