19e1e741747be954bf7132bbf5c764235dcd0794
jnjambrin0 <125131418+jnjambrin0@users.noreply.github.com>
2023-11-24 20:10:42 +0100

    Enhance the API to asign issues to users

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { IssueSchema } from "@/app/validationSchema";
+import { pathcIssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import delay from "delay";
 import { getServerSession } from "next-auth";
@@ -9,14 +9,23 @@ export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions);
-
-  if (!session) return NextResponse.json({}, { status: 401 });
+  //const session = await getServerSession(authOptions);
+  //if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = IssueSchema.safeParse(body);
+
+  const validation = pathcIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { assignedToUserId, title, description } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -27,8 +36,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: parseInt(params.id) },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
 
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -2,5 +2,20 @@ import { z } from "zod";
 
 export const IssueSchema = z.object({
   title: z.string().min(1, "Title is requered").max(255),
-  description: z.string().min(1, "Description is requered"),
+  description: z.string().min(1, "Description is requered").max(65535),
+});
+
+export const pathcIssueSchema = z.object({
+  title: z.string().min(1, "Title is requered").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is requered")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "Assigned to is requered")
+    .max(255)
+    .optional()
+    .nullable(),
 });
